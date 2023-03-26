4a308809993535a0da463f768a91ad3a1d6a5d41
chungminhluan <luanluanluan200797@gmail.com>
2023-03-26 18:22:41 +0700

    add

--- assessment-intern/src/Pages/Login/Login.js
+++ assessment-intern/src/Pages/Login/Login.js
@@ -1,42 +1,76 @@
 import './Login.css';
-import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+// import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 function Login() {
-    const { register, handleSubmit } = useForm(true);
-    const onSubmit = (data) => console.log(data);
+    const [state, setState] = useState({
+        email: '',
+        password: '',
+    });
     const navigate = useNavigate();
 
-    const handleClickLogin = () => {
-        if (register) {
-            navigate('/profile');
-        } else alert('Lỗi');
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setState((prevProps) => ({
+            ...prevProps,
+            [name]: value,
+        }));
     };
 
-    
+    const handleSubmit = () => {
+        // event.preventDefault();
+        if (state) {
+            navigate('/profile');
+        } else alert(123);
+    };
+
+    // const {
+    //     register,
+    //     handleSubmit,
+
+    // } = useForm(true);
+    // const onSubmit = (data) => console.log(data);
+    // const navigate = useNavigate();
+
+    // const handleClickLogin = () => {
+    //     if (register) {
+    //         navigate('/profile');
+    //     } else alert('Lỗi');
+    // };
+
+    // const data = [
+    //     {
+    //         email: ,
+    //         password: '',
+    //     },
+    // ];
     return (
         <div className={'login'}>
             <div className={'container'}>
                 <div className={'card-login'}>
                     <h2 className={'title-login'}>Login</h2>
-                    <form className={'form-login'} onSubmit={handleSubmit(onSubmit)}>
+                    <form className={'form-login'} onSubmit={handleSubmit}>
                         <div className={'email-item'}>
                             <h3 className={'email'}>Email:</h3>
                             <input
-                                {...register('')}
+                                value={state.email}
+                                name="email"
                                 className={'email-input'}
                                 type={'email'}
                                 placeholder={'Example@kyanon.digital'}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className={'password-item'}>
                             <h3 className={'password'}>Password:</h3>
                             <input
-                                {...register}
+                                value={state.password}
+                                name="password"
                                 className={'password-input'}
                                 type={'password'}
                                 placeholder={'******'}
                                 id={'ipnPassword'}
-                                // onChange={handlePassword((e) => setPassword(e.target.value))}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className={'btn-password'}>
@@ -44,14 +78,14 @@ function Login() {
                         </div>
                         <div className={'remember-container'}>
                             <label className={'checkbox-label'}>
-                                <input className={'checkbox-input'} type={'checkbox'} checked />
+                                <input className={'checkbox-input'} type={'checkbox'} />
                                 <span className={'checkmark'}>
                                     <i className={''}></i>
                                 </span>
                                 <p>Show password</p>
                             </label>
 
-                            <button className={'btn-signIn'} onClick={handleClickLogin}>
+                            <button className={'btn-signIn'} type="submit">
                                 Sign in
                             </button>
                         </div>
